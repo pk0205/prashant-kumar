cae16e6758bd4cb9cebd83de0bb3b6537aedddef
Prashant Kumar <kumarprashant670@gmail.com>
2021-01-11 14:28:52 +0530

    Update main.js
    form submission

--- assets/js/main.js
+++ assets/js/main.js
@@ -191,3 +191,49 @@
   });
 
 })(jQuery);
+
+
+window.addEventListener("DOMContentLoaded", function () {
+  // get the form elements defined in your form HTML above
+
+  var form = document.getElementById("my-form");
+  var status = document.getElementById("my-form-status");
+
+  // Success and Error functions for after the form is submitted
+
+  function success() {
+    form.reset();
+    status.innerHTML = "Thanks! Your message has been sent";
+    status.addClass("sent-message");
+  }
+
+  function error() {
+    status.innerHTML = "Oops! There was a problem.";
+    status.addClass("error-message");
+  }
+
+  // handle the form submission event
+
+  form.addEventListener("submit", function (ev) {
+    ev.preventDefault();
+    var data = new FormData(form);
+    ajax(form.method, form.action, data, success, error);
+  });
+});
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
+}
